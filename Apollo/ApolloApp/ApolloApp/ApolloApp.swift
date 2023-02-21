a1f7c91f9fa806823cb89cca61057a9524005a2c
Nikola Grujić <grujic.nikola91@gmail.com>
2023-02-21 06:45:10 +0100

    Reorganized RootView

--- Apollo/ApolloApp/ApolloApp/ApolloApp.swift
+++ Apollo/ApolloApp/ApolloApp/ApolloApp.swift
@@ -7,24 +7,12 @@
 
 import SwiftUI
 import ApolloUI
-import ApolloTheme
-import ApolloLocalization
 
 @main
 struct ApolloApp: App {
-    @StateObject var theme = ThemeManager()
-    @StateObject var localization = LocalizationManager()
-
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .environmentObject(theme)
-                .environmentObject(localization)
-                .onAppear {
-                    theme.load()
-                    localization.load()
-                }
-                .preferredColorScheme(theme.colorScheme)
+            ApolloRootView()
         }
     }
 }
--- Apollo/ApolloUI/Sources/ApolloUI/Interface/RootView.swift
+++ Apollo/ApolloUI/Sources/ApolloUI/Interface/RootView.swift
@@ -9,17 +9,27 @@ import SwiftUI
 import ApolloTheme
 import ApolloLocalization
 
-public struct RootView: View {
+public struct ApolloRootView: View {
+    @StateObject var theme = ThemeManager()
+    @StateObject var localization = LocalizationManager()
+
     public init() {}
 
     public var body: some View {
         MobileMenuView()
+            .environmentObject(theme)
+            .environmentObject(localization)
+            .onAppear {
+                theme.load()
+                localization.load()
+            }
+            .preferredColorScheme(theme.colorScheme)
     }
 }
 
 struct RootView_Previews: PreviewProvider {
     static var previews: some View {
-        RootView()
+        ApolloRootView()
             .environmentObject(ThemeManager())
             .environmentObject(LocalizationManager())
     }
