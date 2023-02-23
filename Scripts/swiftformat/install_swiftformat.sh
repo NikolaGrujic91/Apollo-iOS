7dc4ce4b7978318f3e5950c94275ab30b116f196
Nikola Grujić <grujic.nikola91@gmail.com>
2023-02-23 06:47:24 +0100

    Added Swiftformat

---
+++ Scripts/swiftformat/install_swiftformat.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# https://github.com/nicklockwood/SwiftFormat
+# A command-line tool and Xcode Extension for formatting Swift code.
+
+#chmod +x ./install_swiftformat.sh
+#./install_swiftformat.sh
+
+brew install swiftformat
+
+#brew upgrade swiftformat
---
+++ Scripts/swiftformat/run_swiftformat.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#chmod +x ./run_swiftformat.sh
+#./run_swiftformat.sh
+
+swiftformat --verbose "../../Apollo" --exclude "../../Apollo/Build" --swiftversion 5.7
