e28aa773d96fd90933d77923ee5c994e75902859
Nikola Grujić <grujic.nikola91@gmail.com>
2023-07-25 07:09:12 +0200

    Added UI test for making screenshots: testSnapshotMaker

--- Apollo/ApolloApp/ApolloAppUITests/ApolloAppUITests.swift
+++ Apollo/ApolloApp/ApolloAppUITests/ApolloAppUITests.swift
@@ -8,34 +8,30 @@
 import XCTest
 
 final class ApolloAppUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    func testSnapshotMaker() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
+        setupSnapshot(app)
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        snapshot("1. Plans")
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        app.collectionViews.staticTexts["Weight Loss: Level 1"].tap()
+        snapshot("2. Weeks")
+
+        app.collectionViews.staticTexts["Week 1"].tap()
+        snapshot("3. Days")
+
+        app.collectionViews.buttons["Day 1"].tap()
+        snapshot("4. Timer")
+
+        //app.alerts["Allow “Apollo” to use your location?"].scrollViews.otherElements.buttons["Allow Once"].tap()
+        app.buttons["Play"].tap()
+        app.buttons["Pause"].tap()
+        snapshot("5. Timer")
+
+        app.navigationBars["Day 1"].buttons["Weight Loss: Level 1"].tap()
+        app.tabBars["Tab Bar"].buttons["Settings"].tap()
+        snapshot("6. Settings")
     }
 }
