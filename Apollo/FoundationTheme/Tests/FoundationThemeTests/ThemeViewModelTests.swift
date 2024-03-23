e85c83e3b211e7519110eb7c99c99827403b4848
Nikola Grujić <grujic.nikola91@gmail.com>
2024-03-23 10:13:50 +0100

    Improved FoundationTheme tests

--- Apollo/FoundationTheme/Tests/FoundationThemeTests/ThemeViewModelTests.swift
+++ Apollo/FoundationTheme/Tests/FoundationThemeTests/ThemeViewModelTests.swift
@@ -1,19 +1,50 @@
+//
+//  ThemeViewModelTests.swift
+//
+//
+//  Created by Nikola Grujic on 23/03/2024.
+//
+
+
 @testable import FoundationTheme
 import XCTest
 
 final class ThemeViewModelTests: XCTestCase {
-    func testThemeViewModel() {
-        let viewModel = ThemeViewModel()
+    // MARK: - Properties
 
-        XCTAssertNil(viewModel.colorScheme)
+    private var themeViewModel: ThemeViewModel?
 
-        viewModel.load()
-        XCTAssertEqual(viewModel.colorScheme, .light)
+    // MARK: Functions
 
-        viewModel.save(.dark)
-        XCTAssertEqual(viewModel.colorScheme, .dark)
+    @MainActor
+    override func setUp() {
+        super.setUp()
+        themeViewModel = ThemeViewModel()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
 
-        viewModel.save(.light)
-        XCTAssertEqual(viewModel.colorScheme, .light)
+    @MainActor
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        themeViewModel = nil
+        super.tearDown()
+    }
+
+    // MARK: - Tests
+
+    @MainActor
+    func testOperations() {
+        XCTAssertNotNil(themeViewModel)
+
+        XCTAssertNil(themeViewModel?.colorScheme)
+
+        themeViewModel?.load()
+        XCTAssertEqual(themeViewModel?.colorScheme, .light)
+
+        themeViewModel?.save(.dark)
+        XCTAssertEqual(themeViewModel?.colorScheme, .dark)
+
+        themeViewModel?.save(.light)
+        XCTAssertEqual(themeViewModel?.colorScheme, .light)
     }
 }
